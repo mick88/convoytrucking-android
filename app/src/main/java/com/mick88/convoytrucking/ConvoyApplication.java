ea23b2b32ce1cb575fe1047e2777c686620b918a
michaldabski <contact@michaldabski.com>
2015-11-03 19:19:45 +0000

    Add empty project

---
+++ app/src/main/java/com/mick88/convoytrucking/ConvoyApplication.java
@@ -0,0 +1,9 @@
+package com.mick88.convoytrucking;
+
+import android.app.Application;
+
+/**
+ * Created by Michal on 03/11/2015.
+ */
+public class ConvoyApplication extends Application {
+}
---
+++ app/src/main/java/com/mick88/convoytrucking/api/ApiConstants.java
@@ -0,0 +1,7 @@
+package com.mick88.convoytrucking.api;
+
+/**
+ * Created by Michal on 03/11/2015.
+ */
+public class ApiConstants {
+}
