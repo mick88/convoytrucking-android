256d77b91a3e91d3ffd37bd505676c3251a228f0
michaldabski <contact@michaldabski.com>
2015-11-03 19:46:35 +0000

    Define base urls

--- app/src/main/java/com/mick88/convoytrucking/api/ApiConstants.java
+++ app/src/main/java/com/mick88/convoytrucking/api/ApiConstants.java
@@ -1,7 +1,10 @@
 package com.mick88.convoytrucking.api;
 
+import com.mick88.convoytrucking.BuildConfig;
+
 /**
  * Created by Michal on 03/11/2015.
  */
 public class ApiConstants {
+    public static final String BASE_API = BuildConfig.BASE_URL + "api/";
 }
