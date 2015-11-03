c2620a5e0f701014699ae48ded65e60be7b97ad6
michaldabski <contact@michaldabski.com>
2015-11-03 19:39:46 +0000

    Create Main activity

---
+++ app/src/main/java/com/mick88/convoytrucking/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mick88.convoytrucking;
+
+import android.os.Bundle;
+
+import com.mick88.convoytrucking.base.BaseActivity;
+
+/**
+ * Created by Michal on 03/11/2015.
+ */
+public class MainActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/mick88/convoytrucking/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.mick88.convoytrucking.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Michal on 03/11/2015.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+}
