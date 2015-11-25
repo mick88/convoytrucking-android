e26bbf787cedd8199e25e3681d159cda71f35ca4
michaldabski <contact@michaldabski.com>
2015-11-25 22:22:37 +0000

    Add image loader and bitmap cache

--- app/src/main/java/com/mick88/convoytrucking/api/VolleySingleton.java
+++ app/src/main/java/com/mick88/convoytrucking/api/VolleySingleton.java
@@ -3,7 +3,9 @@ package com.mick88.convoytrucking.api;
 import android.content.Context;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
+import com.mick88.convoytrucking.utils.BitmapCache;
 
 /**
  * Created by lduffy on 17/04/15.
@@ -12,6 +14,8 @@ public class VolleySingleton {
     private static VolleySingleton instance;
     private RequestQueue requestQueue;
     private Context context;
+    private ImageLoader.ImageCache imageCache;
+    private ImageLoader imageLoader;
 
     private VolleySingleton(Context context) {
         this.context = context.getApplicationContext();
@@ -30,5 +34,18 @@ public class VolleySingleton {
             requestQueue = Volley.newRequestQueue(context.getApplicationContext());
         }
         return requestQueue;
+    }
+
+    public ImageLoader.ImageCache getImageCache() {
+        if (imageCache == null)
+            imageCache = new BitmapCache();
+        return imageCache;
+    }
+
+    public ImageLoader getImageLoader() {
+        if (imageLoader == null) {
+            imageLoader = new ImageLoader(getRequestQueue(), getImageCache());
+        }
+        return imageLoader;
     }
 }
