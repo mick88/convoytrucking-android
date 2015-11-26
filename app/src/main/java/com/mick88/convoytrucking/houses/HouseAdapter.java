b2971da462482338be184d11b7f78b6fa1c30a5a
michaldabski <contact@michaldabski.com>
2015-11-26 23:01:05 +0000

    Replace listview with a recycler view

--- app/src/main/java/com/mick88/convoytrucking/houses/HouseAdapter.java
+++ app/src/main/java/com/mick88/convoytrucking/houses/HouseAdapter.java
@@ -1,15 +1,12 @@
 package com.mick88.convoytrucking.houses;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.NetworkImageView;
 import com.mick88.convoytrucking.R;
 import com.mick88.convoytrucking.api.VolleySingleton;
 import com.mick88.convoytrucking.api.schema.models.House;
@@ -18,45 +15,35 @@ import com.mick88.convoytrucking.utils.FormatUtils;
 /**
  * Created by Michal on 25/11/2015.
  */
-public class HouseAdapter extends ArrayAdapter<House> {
-
-    private static class ViewHolder {
-        NetworkImageView imageView;
-        TextView tvAddress;
-        TextView tvPrice, tvSlots, tvOwner;
-        ImageView imgIcon;
-    }
-
-    private ImageLoader imageLoader;
+public class HouseAdapter extends RecyclerView.Adapter<HouseViewHolder> {
+    protected final House[] houses;
+    protected final ImageLoader imageLoader;
+    protected final Context context;
 
     public HouseAdapter(Context context, House[] houses) {
-        super(context, 0, houses);
-        imageLoader = VolleySingleton.getInstance(getContext()).getImageLoader();
+        super();
+        this.houses = houses;
+        this.context = context;
+        imageLoader = VolleySingleton.getInstance(context).getImageLoader();
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        final ViewHolder holder;
-        if (view == null) {
-            view = LayoutInflater.from(getContext()).inflate(R.layout.card_house, parent, false);
-            view.setTag(holder = new ViewHolder());
-            holder.imageView = (NetworkImageView) view.findViewById(R.id.image);
-            holder.tvAddress = (TextView) view.findViewById(R.id.tvAddress);
-            holder.tvPrice = (TextView) view.findViewById(R.id.tvPrice);
-            holder.tvSlots = (TextView) view.findViewById(R.id.tvSlots);
-            holder.tvOwner = (TextView) view.findViewById(R.id.tvOwner);
-            holder.imgIcon = (ImageView) view.findViewById(R.id.icon);
-        } else {
-            holder = (ViewHolder) view.getTag();
-        }
+    public HouseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_house, parent, false);
+        return new HouseViewHolder(view);
+    }
 
-        final House house = getItem(position);
+    @Override
+    public void onBindViewHolder(HouseViewHolder holder, int position) {
+        final House house = houses[position];
+
+        holder.currentItem = house;
 
         holder.imageView.setDefaultImageResId(R.drawable.placeholder_house);
         holder.imageView.setImageUrl(house.getImageUrl(House.IMAGE_SIZE_MEDIUM), imageLoader);
         holder.tvAddress.setText(house.getAddress());
         holder.tvPrice.setText(FormatUtils.formatPrice(house.getPrice()));
-        holder.tvSlots.setText(getContext().getString(R.string.s_slots, house.getParking()));
+        holder.tvSlots.setText(context.getString(R.string.s_slots, house.getParking()));
         if (house.isForSale()) {
             holder.imgIcon.setImageResource(R.drawable.ic_house_forsale);
             holder.tvOwner.setText(null);
@@ -64,6 +51,10 @@ public class HouseAdapter extends ArrayAdapter<House> {
             holder.imgIcon.setImageResource(R.drawable.ic_house_owned);
             holder.tvOwner.setText(house.getOwner().getName());
         }
-        return view;
+    }
+
+    @Override
+    public int getItemCount() {
+        return houses.length;
     }
 }
--- app/src/main/java/com/mick88/convoytrucking/houses/HouseListFragment.java
+++ app/src/main/java/com/mick88/convoytrucking/houses/HouseListFragment.java
@@ -1,24 +1,18 @@
 package com.mick88.convoytrucking.houses;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.mick88.convoytrucking.R;
 import com.mick88.convoytrucking.api.ModelRequest;
 import com.mick88.convoytrucking.api.schema.feeds.HouseFeed;
-import com.mick88.convoytrucking.api.schema.models.House;
 import com.mick88.convoytrucking.base.ApiFragment;
-import com.mick88.convoytrucking.houses.detail.HouseActivity;
 
 /**
  * Created by Michal on 25/11/2015.
  */
-public class HouseListFragment extends ApiFragment<HouseFeed> implements AdapterView.OnItemClickListener {
+public class HouseListFragment extends ApiFragment<HouseFeed> {
     @Override
     protected Class<HouseFeed> getModelClass() {
         return HouseFeed.class;
@@ -31,25 +25,13 @@ public class HouseListFragment extends ApiFragment<HouseFeed> implements Adapter
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        final ListView listView = (ListView) view.findViewById(R.id.listView);
-        listView.setOnItemClickListener(this);
-    }
-
-    @Override
     public void onResponse(HouseFeed response) {
         super.onResponse(response);
-        final ArrayAdapter<House> houseAdapter = new HouseAdapter(getContext(), response.getResults());
-
-        final ListView listView = (ListView) getView().findViewById(R.id.listView);
-        listView.setAdapter(houseAdapter);
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        final House house = (House) parent.getItemAtPosition(position);
-        Intent intent = HouseActivity.newIntent(getContext(), house.getId());
-        startActivity(intent);
+        final HouseAdapter houseAdapter = new HouseAdapter(getContext(), response.getResults());
+        final RecyclerView recyclerView = (RecyclerView) getView().findViewById(R.id.recyclerView);
+        final RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(houseAdapter);
     }
 }
