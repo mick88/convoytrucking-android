e668a969bb4a5545d0936c95f616c1b7505ea6eb
michaldabski <contact@michaldabski.com>
2015-11-29 15:49:27 +0000

    Add staff list to server info

---
+++ app/src/main/java/com/mick88/convoytrucking/api/schema/feeds/PlayerFeed.java
@@ -0,0 +1,9 @@
+package com.mick88.convoytrucking.api.schema.feeds;
+
+import com.mick88.convoytrucking.api.schema.models.Player;
+
+/**
+ * Created by Michal on 29/11/2015.
+ */
+public class PlayerFeed extends BasePaginatedFeed<Player> {
+}
--- app/src/main/java/com/mick88/convoytrucking/server_info/ServerInfoFragment.java
+++ app/src/main/java/com/mick88/convoytrucking/server_info/ServerInfoFragment.java
@@ -1,24 +1,34 @@
 package com.mick88.convoytrucking.server_info;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.android.volley.Response;
 import com.mick88.convoytrucking.R;
 import com.mick88.convoytrucking.api.ApiConstants;
+import com.mick88.convoytrucking.api.ModelRequest;
+import com.mick88.convoytrucking.api.schema.feeds.PlayerFeed;
+import com.mick88.convoytrucking.api.schema.models.Player;
 import com.mick88.convoytrucking.api.schema.models.ServerInfo;
+import com.mick88.convoytrucking.api.schema.models.SimplePlayer;
 import com.mick88.convoytrucking.base.ApiFragment;
+import com.mick88.convoytrucking.players.PlayerActivity;
 
 import java.util.Locale;
 
 /**
  * Created by Michal on 21/11/2015.
  */
-public class ServerInfoFragment extends ApiFragment<ServerInfo> {
+public class ServerInfoFragment extends ApiFragment<ServerInfo> implements AdapterView.OnItemClickListener {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,6 +50,32 @@ public class ServerInfoFragment extends ApiFragment<ServerInfo> {
     public void onResponse(ServerInfo info) {
         super.onResponse(info);
         setServerInfo(info);
+    }
+
+    @Override
+    protected void sendRequest() {
+        super.sendRequest();
+        fetchStaff();
+    }
+
+    protected void fetchStaff() {
+        final String url = ApiConstants.API_PLAYERS + "?mod__gte=" + Player.MOD_JRMOD;
+        ModelRequest<PlayerFeed> request = new ModelRequest<>(url, PlayerFeed.class, this, new Response.Listener<PlayerFeed>() {
+            @Override
+            public void onResponse(PlayerFeed response) {
+                final ArrayAdapter<Player> playerAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, response.getResults());
+                final ListView listView = (ListView) getView().findViewById(R.id.listView);
+                listView.setAdapter(playerAdapter);
+            }
+        });
+        sendRequest(request);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final ListView staffListView = (ListView) view.findViewById(R.id.listView);
+        staffListView.setOnItemClickListener(this);
     }
 
     protected void setServerInfo(ServerInfo serverInfo) {
@@ -84,5 +120,15 @@ public class ServerInfoFragment extends ApiFragment<ServerInfo> {
                 break;
         }
         tvServerAddress.setCompoundDrawablesWithIntrinsicBounds(0, 0, icon, 0);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        final Object item = parent.getItemAtPosition(position);
+        if (item instanceof SimplePlayer) {
+            SimplePlayer player = ((SimplePlayer) item);
+            final Intent intent = PlayerActivity.createIntent(getContext(), player);
+            startActivity(intent);
+        }
     }
 }
