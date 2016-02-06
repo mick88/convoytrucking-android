145873102f55a47b838e7186b1458ffea3d02bc8
michaldabski <contact@michaldabski.com>
2016-02-06 00:17:01 +0000

    Create main activity test

---
+++ app/src/androidTest/java/com/mick88/convoytrucking/MainActivityInstrumentationTest.java
@@ -0,0 +1,30 @@
+package com.mick88.convoytrucking;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Michal on 05/02/2016.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityInstrumentationTest {
+    MainActivity activity;
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        activity = activityTestRule.getActivity();
+    }
+
+    @Test
+    public void testActivityLaunch() throws Exception {
+        assert activity != null;
+    }
+}
